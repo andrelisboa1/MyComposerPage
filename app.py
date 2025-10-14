396c23c2c2c234fd93d191bea3307dd0812b6fab
andrelisboa1 <51169222+andrelisboa1@users.noreply.github.com>
2025-10-14 14:03:10 +0000

    nav

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", current_year=datetime.now().year)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", current_year=datetime.now().year)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
