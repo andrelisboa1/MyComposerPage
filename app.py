a26c910571f8dad4a5356daa17c2bf7da68324e2
andrelisboa1 <51169222+andrelisboa1@users.noreply.github.com>
2025-10-14 13:47:28 +0000

    a

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=4000)
 
 # Old command: gunicorn your_application.wsgi
